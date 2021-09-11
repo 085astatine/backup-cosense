9f6ea69a6a4b1653ba25d052517f7e1b563c1ad6
085astatine <astatine085@gmail.com>
2021-09-11 22:11:22 +0900

    Add logger

--- backup_scrapbox.py
+++ backup_scrapbox.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 
+import logging
+from typing import Optional
 
-def backup_scrapbox() -> None:
-    pass
+
+def backup_scrapbox(
+        *,
+        logger: Optional[logging.Logger] = None) -> None:
+    logger = logger or logging.getLogger(__name__)
+    logger.info('backup-scrapbox')
 
 
 if __name__ == '__main__':
-    backup_scrapbox()
+    # logger
+    logger = logging.getLogger('backup-scrapbox')
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    handler.formatter = logging.Formatter(
+            fmt='%(asctime)s %(name)s:%(levelname)s:%(message)s')
+    logger.addHandler(handler)
+    # main
+    backup_scrapbox(logger=logger)
