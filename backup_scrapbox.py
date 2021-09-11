efa116f581c12d7b29a765b4a6e6f8ddfbf878f2
085astatine <astatine085@gmail.com>
2021-09-11 22:22:00 +0900

    Add argument parser

--- backup_scrapbox.py
+++ backup_scrapbox.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import logging
 from typing import Optional
 
@@ -11,6 +12,17 @@ def backup_scrapbox(
     logger.info('backup-scrapbox')
 
 
+def argument_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    # verbose
+    parser.add_argument(
+            '-v', '--verbose',
+            dest='verbose',
+            action='store_true',
+            help='set log level to debug')
+    return parser
+
+
 if __name__ == '__main__':
     # logger
     logger = logging.getLogger('backup-scrapbox')
@@ -19,5 +31,10 @@ if __name__ == '__main__':
     handler.formatter = logging.Formatter(
             fmt='%(asctime)s %(name)s:%(levelname)s:%(message)s')
     logger.addHandler(handler)
+    # option
+    option = argument_parser().parse_args()
+    if option.verbose:
+        logger.setLevel(logging.DEBUG)
+    logger.debug('option: %s', option)
     # main
     backup_scrapbox(logger=logger)
