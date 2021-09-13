fff7e70ab51c43ccd28d2547260e017a68ba79c0
085astatine <astatine085@gmail.com>
2021-09-13 20:45:38 +0900

    Add load_json() and save_json()

--- backup_scrapbox/_json.py
+++ backup_scrapbox/_json.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from typing import TypedDict
+import json
+import pathlib
+from typing import Any, Optional, TypedDict
 
 
 class BackupInfoJSON(TypedDict):
@@ -44,3 +46,25 @@ def jsonschema_backup_list():
       },
     }
     return schema
+
+
+def load_json(
+        path: pathlib.Path) -> Optional[Any]:
+    if not path.exists():
+        return None
+    with path.open() as file:
+        return json.load(file)
+
+
+def save_json(
+        path: pathlib.Path,
+        data: Any) -> None:
+    if not path.parent.exists():
+        path.parent.mkdir(parents=True)
+    with path.open(mode='w') as file:
+        json.dump(
+                data,
+                file,
+                ensure_ascii=False,
+                indent=2)
+        file.write('\n')
