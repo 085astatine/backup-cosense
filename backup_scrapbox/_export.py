320bfa0e804ed839d4f9c285975aedc68bf99755
085astatine <astatine085@gmail.com>
2023-02-13 14:07:21 +0900

    Get backup.info.json from git repository

--- backup_scrapbox/_export.py
+++ backup_scrapbox/_export.py
@@ -2,7 +2,7 @@ import logging
 import pathlib
 import subprocess
 from typing import Any, Optional
-from ._backup import jsonschema_backup
+from ._backup import jsonschema_backup, jsonschema_backup_info
 from ._config import Config
 from ._git import Git, Commit
 from ._json import parse_json, save_json
@@ -56,11 +56,20 @@ def _export(
         return
     logger.debug(f'save "{backup_path}"')
     # save backup.info.json
-    info_json_path = destination.joinpath(f'{commit.timestamp}.info.json')
-    info_json = commit.backup_info()
-    if info_json is not None:
-        logger.debug(f'save "{info_json_path}"')
-        save_json(info_json_path, info_json)
+    info_path = destination.joinpath(f'{commit.timestamp}.info.json')
+    if _export_json(
+            git,
+            commit.hash,
+            f'{project}.info.json',
+            info_path,
+            jsonschema_backup_info()):
+        logger.debug(f'save "{info_path}"')
+    else:
+        # from commit message
+        info_json = commit.backup_info()
+        if info_json is not None:
+            save_json(info_path, info_json)
+            logger.debug(f'save "{info_path}"')
 
 
 def _export_json(
