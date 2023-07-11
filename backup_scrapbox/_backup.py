518448c947cabf57750ce3b2a74fd5194da69707
085astatine <astatine085@gmail.com>
2023-07-11 17:51:50 +0900

    Update JSONSchema
    Add .users and .pages[].lines[].userId in 2023-04-13.

--- backup_scrapbox/_backup.py
+++ backup_scrapbox/_backup.py
@@ -50,6 +50,7 @@ def jsonschema_backup_page_line() -> dict[str, Any]:
         'text': {'type': 'string'},
         'created': {'type': 'integer'},
         'updated': {'type': 'integer'},
+        'userId': {'type': 'string'},
       },
     }
     return schema
@@ -120,6 +121,10 @@ def jsonschema_backup() -> dict[str, Any]:
         'name': {'type': 'string'},
         'displayName': {'type': 'string'},
         'exported': {'type': 'integer'},
+        'users': {
+          'type': 'array',
+          'items': {'type': 'string'},
+        },
         'pages': {
           'type': 'array',
           'items': jsonschema_backup_page(),
