b99fa54fc354d50cfa7d0dda0d1fcf93ccd7d7c4
085astatine <astatine085@gmail.com>
2025-01-11 23:14:31 +0900

    Add domain to ScrapboxConfig

--- backup_scrapbox/_download.py
+++ backup_scrapbox/_download.py
@@ -56,7 +56,9 @@ def download_backups(
 
 
 def _base_url(config: Config) -> str:
-    return f"https://scrapbox.io/api/project-backup/{config.scrapbox.project}"
+    domain = config.scrapbox.domain
+    project = config.scrapbox.project
+    return f"https://{domain}/api/project-backup/{project}"
 
 
 def _session(config: Config) -> requests.Session:
