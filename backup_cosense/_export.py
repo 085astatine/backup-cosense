272464ab1e6569af95c0091e7f4c6960f62d632e
Astatine <astatine085@gmail.com>
2025-11-05 21:15:32 +0900

    Change export_backups(logger=...) to a keyword argument

--- backup_cosense/_export.py
+++ backup_cosense/_export.py
@@ -18,12 +18,13 @@ from ._utility import format_timestamp
 def export_backups(
     config: Config,
     destination: BackupArchive,
-    logger: logging.Logger,
     *,
     dry_run: bool = False,
     after: Optional[datetime.datetime] = None,
     before: Optional[datetime.datetime] = None,
+    logger: Optional[logging.Logger] = None,
 ) -> None:
+    logger = logger or logging.getLogger(__name__)
     git = config.git.create(logger=logger)
     # check if the destination exists
     if not destination.path.exists():
